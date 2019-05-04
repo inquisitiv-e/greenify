d2ee2532eb9d87d1bd8a124f57737c6e27c6cfd6
gymdw140472 <dseo9@my.bcit.ca>
2019-05-04 16:05:11 -0700

    user auth

--- public/js/userAuth.js
+++ public/js/userAuth.js
@@ -18,21 +18,7 @@ function fbLogin() {
   promise.then(e => emsg.innerHTML = "");
 }
 
-// //Add log in event
-// btnLogin.addEventListener('click', e => {
-//   //get email and ps
-//   const email = txtEmail.value;
-//   const pass = txtPassword.value;
-//   const auth = firebase.auth();
-//   //sign in
-//   const promise = auth.signInWithEmailAndPassword(email, pass);
-//   //check validation
-//   promise.catch(e => emsg.innerHTML = e.message);
-//   promise.then(e => emsg.innerHTML = "");
-// });
-
-//Add sign up event
-btnSignup.addEventListener('click', e => {
+function fbSignup() {
   //get email and ps
   const email = txtEmail.value;
   const pass = txtPassword.value;
@@ -50,12 +36,49 @@ btnSignup.addEventListener('click', e => {
       name: uname,
     })
   });
-});
+}
+
+function fbLogout() {
+  firebase.auth().signOut();
+}
+// //Add log in event
+// btnLogin.addEventListener('click', e => {
+//   //get email and ps
+//   const email = txtEmail.value;
+//   const pass = txtPassword.value;
+//   const auth = firebase.auth();
+//   //sign in
+//   const promise = auth.signInWithEmailAndPassword(email, pass);
+//   //check validation
+//   promise.catch(e => emsg.innerHTML = e.message);
+//   promise.then(e => emsg.innerHTML = "");
+// });
+
+//Add sign up event
+// btnSignup.addEventListener('click', e => {
+//   //get email and ps
+//   const email = txtEmail.value;
+//   const pass = txtPassword.value;
+//   const uname = userName.value;
+//   const auth = firebase.auth();
+//   //sign in
+//   const promise = auth.createUserWithEmailAndPassword(email, pass);
+//   //check validation
+//   promise.catch(e => emsg.innerHTML = e.message);
+//   promise.then(e => {
+//     emsg.innerHTML = "";
+//     var user = firebase.auth().currentUser;
+//     console.log(uname);
+//     user.updateProfile({
+//       name: uname,
+//     })
+//   });
+// });
 
 //Add log out event
-btnLogout.addEventListener('click', e => {
-  firebase.auth().signOut();
-});
+// btnLogout.addEventListener('click', e => {
+//   firebase.auth().signOut();
+// });
 
 firebase.auth().onAuthStateChanged(firebaseUser => {
   if (firebaseUser) {
