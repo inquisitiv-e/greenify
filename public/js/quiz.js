5cec2f47c1775fac03512e67a84690ead6ebc795
Hannah Chung <hyerang1205@gmail.com>
2019-05-05 11:03:28 -0700

    quiz

---
+++ public/js/quiz.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+// var firebaseConfig = {
+//   apiKey: "AIzaSyCACZPJJ4gnsfceKLEpqQPpm1JDNDVKarY",
+//   authDomain: "clime-b5f97.firebaseapp.com",
+//   databaseURL: "https://clime-b5f97.firebaseio.com",
+//   projectId: "clime-b5f97",
+//   storageBucket: "clime-b5f97.appspot.com",
+//   messagingSenderId: "334342587603",
+//   appId: "1:334342587603:web:54478d2843b875c9"
+// };
+// firebase.initializeApp(firebaseConfig);
+
+console.log(firebase);
+
+var ref = database.ref('quiz');
+//
+// var data = {
+//     name: "Hannah",
+//     quiz: "today?"
+// };
+//
+// ref.push(data);
+
+
+});
+// var database = firebase.database();
+// // Import Admin SDK
+// var admin = require("firebase-admin");
+//
+// // Get a database reference to our posts
+// var db = admin.database();
+// var ref = db.ref("server/saving-data/fireblog/posts");
+//
+// // Attach an asynchronous callback to read the data at our posts reference
+// ref.on("value", function(snapshot) {
+//   console.log(snapshot.val());
+// }, function (errorObject) {
+//   console.log("The read failed: " + errorObject.code);
+// });
